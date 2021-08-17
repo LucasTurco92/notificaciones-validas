586f9f6d018ebee5f43c3eaca8a7fcc1c1471bc4
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2021-08-17 03:01:00 -0300

    third section refactor

--- src/components/section-third/section-third.jsx
+++ src/components/section-third/section-third.jsx
@@ -13,32 +13,30 @@ const SectionThird = () => {
              {/* <div className={styles.cardsContainer}>
                 <Cards/>
             </div> */}
+         
           <div className={styles.box}>
-            <div className={styles.image}>
-              <img className={styles.bell} src={'/images/hand-shake-1.jpg'} />
-            </div>
+
             <div className={styles.text}>
               <b>Como funciona?</b>
-              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pulvinar maecenas cursus, pellentesque dapibus sociosqu feugiat mollis sodales aliquam velit taciti, enim curabitur laoreet tempor lacinia interdum porttitor convallis diam. Hac quisque pharetra malesuada nam pretium potenti vehicula magna, laoreet mattis vivamus sollicitudin non pellentesque habitant senectus nisl, ad suscipit purus quam blandit cursus sociis. Ad tortor conubia ac posuere feugiat sem ultricies eget natoque malesuada vestibulum, pellentesque curabitur cras tempor augue felis sollicitudin mauris vulputate.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pulvinar maecenas cursus, pellentesque dapibus sociosqu feugiat mollis sodales aliquam velit taciti, enim curabitur laoreet tempor lacinia interdum porttitor convallis diam. Hac quisque pharetra malesuada nam pretium potenti vehicula magna, laoreet mattis vivamus sollicitudin non pellente.</p>
             </div>
           </div>
-          <div className={styles.boxInverted}>
-
-            <div className={styles.image}>
-              <img className={styles.bell} src={'/images/contract-1.jpg'} />
-            </div>
-            <div className={styles.text}>
-              <b>Como funciona?</b>
-              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pulvinar maecenas cursus, pellentesque dapibus sociosqu feugiat mollis sodales aliquam velit taciti, enim curabitur laoreet tempor lacinia interdum porttitor convallis diam. Hac quisque pharetra malesuada nam pretium potenti vehicula magna, laoreet mattis vivamus sollicitudin non pellentesque habitant senectus nisl, ad suscipit purus quam blandit cursus sociis. Ad tortor conubia ac posuere feugiat sem ultricies eget natoque malesuada vestibulum, pellentesque curabitur cras tempor augue felis sollicitudin mauris vulputate.</p>
-            </div>
-            </div>
           <div className={styles.box}>
-            <div className={styles.image}>
-              <img className={styles.bell} src={'/images/real-time-1.jpg'} />
-            </div>
             <div className={styles.text}>
               <b>Como funciona?</b>
-              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pulvinar maecenas cursus, pellentesque dapibus sociosqu feugiat mollis sodales aliquam velit taciti, enim curabitur laoreet tempor lacinia interdum porttitor convallis diam. Hac quisque pharetra malesuada nam pretium potenti vehicula magna, laoreet mattis vivamus sollicitudin non pellentesque habitant senectus nisl, ad suscipit purus quam blandit cursus sociis. Ad tortor conubia ac posuere feugiat sem ultricies eget natoque malesuada vestibulum, pellentesque curabitur cras tempor augue felis sollicitudin mauris vulputate.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pulvinar maecenas cursus, pellentesque dapibus sociosqu feugiat mollis sodales aliquam velit taciti, enim curabitur laoreet tempor lacinia interdum porttitor convallis diam. Hac quisque pharetra malesuada nam pretium potenti vehicula magna, laoreet mattis vivamus sollicitudin non pellente.</p>
+            </div>
+          </div>
+          <div className={styles.box}>
+            <div className={styles.text}>
+              <b>Como funciona?</b>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pulvinar maecenas cursus, pellentesque dapibus sociosqu feugiat mollis sodales aliquam velit taciti, enim curabitur laoreet tempor lacinia interdum porttitor convallis diam. Hac quisque pharetra malesuada nam pretium potenti vehicula magna, laoreet mattis vivamus sollicitudin non pellente.</p>
+            </div>
+          </div>
+          <div className={styles.box}>
+            <div className={styles.text}>
+              <b>Como funciona?</b>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit pulvinar maecenas cursus, pellentesque dapibus sociosqu feugiat mollis sodales aliquam velit taciti, enim curabitur laoreet tempor lacinia interdum porttitor convallis diam. Hac quisque pharetra malesuada nam pretium potenti vehicula magna, laoreet mattis vivamus sollicitudin non pellente.</p>
             </div>
           </div>
         </div>
