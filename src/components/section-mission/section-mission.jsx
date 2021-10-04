ac2ffc746e8c678ae7202953d6280f1ca43319e3
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2021-10-04 01:42:01 -0300

    expanded section added

--- src/components/section-mission/section-mission.jsx
+++ src/components/section-mission/section-mission.jsx
@@ -1,29 +1,36 @@
-import { useRef } from 'react';
+import { useRef,useEffect } from 'react';
 import styles from "./section-mission.module.scss";
 import useOnScreen from '../../hooks/intersectionObserver';
 import useResponsiveWindow from '../../hooks/responsiveWindow';
+import Router from 'next/router'
 
 
 const SectionMission = () => {
   const { isDesktop } = useResponsiveWindow();
   const distance = isDesktop ? "-200px" : "";
+  const expandDevice = isDesktop ? styles.expandDesktop: styles.expandMobile;
   const ref = useRef();
   const onScreen = useOnScreen(ref, distance);
 
+  useEffect(()=>{
+    if(onScreen) {Router.push('/#mission')};
+
+  },[onScreen]);
+
   return (
-    <div id={'mission'}  className={styles.content}> 
+    <div id={'mission'}  className={onScreen ? `${styles.content} ${expandDevice}` :styles.content}> 
     
-      <div className={styles.box}>
-        <h2 className={styles.title}>Misión</h2>
+      <div className={styles.box} >
+        <h2  className={styles.title}>Misión</h2>
         <p>
         Crear una plataforma que otorgue VALOR LEGAL a un medio electrónico, de forma rápida, segura y eficaz permitiendo ahorrar tiempo y dinero, evitando problemas legales por el uso de correos electrónicos.
         </p>
-        <p>
+        <p ref={ref}>
         NUNCA FUE TAN FACIL, EFICAZ Y ECONÓMICO NOTIFICAR UNA DECISIÓN.
         </p>
       </div>
       
-      <img ref={ref} className={onScreen ? `${styles.bellAnimation} ${styles.bell}`  : styles.bell } src={'/images/bell-solid.svg'} />
+      <img className={onScreen ? `${styles.bellAnimation} ${styles.bell}`  : styles.bell } src={'/images/bell-solid.svg'} />
     </div>
   )
 }
