c13fed3639d18dcd1502453f0b0b1b52afdec2ac
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2021-10-04 16:17:10 -0300

    useResponsive window hook is removed completely

--- src/components/section-first/section-first.jsx
+++ src/components/section-first/section-first.jsx
@@ -1,15 +1,13 @@
 import styles from './section-first.module.scss';
-import useResponsiveWindow from '../../hooks/responsiveWindow';
 import SeparatorTop from "../separator-top/separator-top";
 
 const SectionFirst = () => {
-    const { isDesktop } = useResponsiveWindow();
     return (
         <div id={'intro'} className={styles.container}> 
             <div className={styles.cover} >
-                { isDesktop ? <img className={styles.coverUpDesktop} src={'/images/galaxy-2.jpg'} />
-                : <img className={styles.coverUpMobile} src={'/images/galaxy-mobile.jpg'} />}
-           
+                 <img className={styles.coverUpDesktop} src={'/images/galaxy-2.jpg'} />
+                 <img className={styles.coverUpMobile} src={'/images/galaxy-mobile.jpg'} />}
+            
                 <div className={styles.coverImageContainer}>
                     <h2 className={styles.title}>Notificaciones Válidas</h2>        
                     <div className={styles.coverImage}>
