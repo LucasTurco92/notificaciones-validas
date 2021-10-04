0f0edb69fdb92b907040c07e5b21b418f55d12d1
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2021-10-03 23:17:32 -0300

    intersectionObserver and bell animation are added

--- src/components/section-mission/section-mission.jsx
+++ src/components/section-mission/section-mission.jsx
@@ -1,19 +1,27 @@
+import { useRef } from 'react';
 import styles from "./section-mission.module.scss";
-const SectionMission = () => {
-  return (
-        <div id={'mission'}  className={styles.content}> 
-          <div className={styles.box}>
-            <h2 className={styles.title}>Misión</h2>
-            <p>
-            Crear una plataforma que otorgue VALOR LEGAL a un medio electrónico, de forma rápida, segura y eficaz permitiendo ahorrar tiempo y dinero, evitando problemas legales por el uso de correos electrónicos.
-            </p>
-            <p>
-            NUNCA FUE TAN FACIL, EFICAZ Y ECONÓMICO NOTIFICAR UNA DECISIÓN.
-            </p>
-          </div>
-          <img className={styles.bell} src={'/images/bell-solid.svg'} />
-        </div>
+import useOnScreen from '../../hooks/intersectionObserver';
 
+
+const SectionMission = () => {
+  const ref = useRef();
+  const onScreen = useOnScreen(ref, "-200px");
+console.log('onScreen: ',onScreen);
+  return (
+    <div id={'mission'}  className={styles.content}> 
+    
+      <div className={styles.box}>
+        <h2 className={styles.title}>Misión</h2>
+        <p>
+        Crear una plataforma que otorgue VALOR LEGAL a un medio electrónico, de forma rápida, segura y eficaz permitiendo ahorrar tiempo y dinero, evitando problemas legales por el uso de correos electrónicos.
+        </p>
+        <p>
+        NUNCA FUE TAN FACIL, EFICAZ Y ECONÓMICO NOTIFICAR UNA DECISIÓN.
+        </p>
+      </div>
+      
+      <img ref={ref} className={onScreen ? `${styles.bellAnimation} ${styles.bell}`  : styles.bell } src={'/images/bell-solid.svg'} />
+    </div>
   )
 }
 
